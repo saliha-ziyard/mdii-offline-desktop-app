c013022c49c99f0f7940ea3b779417e9c3972d42
saliha-ziyard <salihaziyard@gmail.com>
2025-08-18 09:31:58 +0530

    unnessasary parts removed

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,4 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './components/App.jsx';
 
-// const root = createRoot(document.getElementById('root'));
-// root.render(<App />);
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 // Make sure the case matches your file name exactly
