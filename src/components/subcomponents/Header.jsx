d589732c1f542a09861d70126adaa6491f998b53
saliha-ziyard <salihaziyard@gmail.com>
2025-08-18 09:32:38 +0530

    header modified

--- src/components/subcomponents/Header.jsx
+++ src/components/subcomponents/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Header = () => (
+    <>
     <div className="header">
         <p className="header-title"> MDII Evaluation Tool
         </p>
     </div>
+    <hr/>
+    </>
 );
 
 export default Header;
