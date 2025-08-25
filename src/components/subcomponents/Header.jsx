5539c173e301907afa21ba09c58a38e46b4383ba
saliha-ziyard <salihaziyard@gmail.com>
2025-08-25 13:53:07 +0530

    header logo added

--- src/components/subcomponents/Header.jsx
+++ src/components/subcomponents/Header.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 const Header = () => (
-    <>
+  <>
     <div className="header">
-        <p className="header-title"> MDII Evaluation Tool
-        </p>
+      <p className="header-title">MDII Evaluation Tool</p>
+      <div className="header-logos">
+        <img 
+          src="./images/Wapor_Logo.png" 
+          alt="Wapor Logo" 
+          className="logo"
+        />
+        <img 
+          src="./images/MDII_Logo.png" 
+          alt="MDII Logo" 
+          className="logo"
+        />
+      </div>
     </div>
     <hr/>
-    </>
+  </>
 );
 
 export default Header;
