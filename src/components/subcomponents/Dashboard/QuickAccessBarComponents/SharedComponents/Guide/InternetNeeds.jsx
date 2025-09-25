976dd52f4837a85a4369ff87d195bb6c49f69b7a
saliha-ziyard <salihaziyard@gmail.com>
2025-09-25 17:32:20 +0530

    InternetNeeds updated

--- src/components/subcomponents/Dashboard/QuickAccessBarComponents/SharedComponents/Guide/InternetNeeds.jsx
+++ src/components/subcomponents/Dashboard/QuickAccessBarComponents/SharedComponents/Guide/InternetNeeds.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { GoGlobe } from "react-icons/go";
+import { FaShieldAlt } from "react-icons/fa";
 
 const InternetNeeds = ({ setCurrentPage, setActiveSection, activeSection }) => {
   return (
     <div className="content-body">
       <h3>Internet Needs</h3>
-      <div>
-        <strong>Connection Required For:</strong>
-        <ul>
-          <li>Data fetching during compilation and score generation</li>
-          <li>Opening KoboToolbox form URL for the first time (caching for offline use)</li>
-        </ul>
+      <div className="box grey-box">
+        <b>Connection Required For:</b>
+          <p><FaShieldAlt />Data fetching during compilation and score generation</p>
+          <p><FaShieldAlt />Opening KoboToolbox form URL for the first time (caching for offline use)</p>
+       
       </div>
       <p>
         The MDII Desktop App is designed to work effectively in environments
         with limited or intermittent internet access.
       </p>
 
-      <div className="grey-box">
+      <div className="box green-box">
         <p>For more information on how to use KoboToolbox in offline settings, please consult: </p>
         <p>
-          <a href="https://support.kobotoolbox.org/data_through_webforms.html?highlight=offline">
+          <a
+            href="https://support.kobotoolbox.org/data_through_webforms.html?highlight=offline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             KoboToolbox Offline Documentation
           </a>
+
         </p>
       </div>
     </div>
